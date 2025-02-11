63a27ac5d4d277e7fe93c45e554eefd581d46dfd
ViscusDev <bryn@au.viscushub.com>
2025-02-11 19:52:57 +1100

    Update RailType.java

--- fabric/src/main/java/org/mtr/mod/data/RailType.java
+++ fabric/src/main/java/org/mtr/mod/data/RailType.java
@@ -5,6 +5,7 @@ import org.mtr.mapping.holder.MapColor;
 
 public enum RailType implements IGui {
 	WOODEN(20, MapColor.getOakTanMapped(), false, true, true, Rail.Shape.QUADRATIC),
+	WOODEN2(30, MapColor.getOakTanMapped(), false, true, true, Rail.Shape.QUADRATIC),
 	STONE(40, MapColor.getStoneGrayMapped(), false, true, true, Rail.Shape.QUADRATIC),
 	EMERALD(60, MapColor.getEmeraldGreenMapped(), false, true, true, Rail.Shape.QUADRATIC),
 	IRON(80, MapColor.getIronGrayMapped(), false, true, true, Rail.Shape.QUADRATIC),
