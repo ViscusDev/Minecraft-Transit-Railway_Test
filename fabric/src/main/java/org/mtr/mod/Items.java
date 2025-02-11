1cb673d0045a855018e508f60b6c470f328710b9
ViscusDev <bryn@au.viscushub.com>
2025-02-11 20:16:58 +1100

    Update Items.java

--- fabric/src/main/java/org/mtr/mod/Items.java
+++ fabric/src/main/java/org/mtr/mod/Items.java
@@ -49,6 +49,8 @@ public final class Items {
 		RAIL_CONNECTOR_30_ONE_WAY = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_30_one_way"), itemSettings -> new Item(new ItemRailModifier(true, false, true, true, RailType.WOODEN2, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_40 = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_40"), itemSettings -> new Item(new ItemRailModifier(true, false, true, false, RailType.STONE, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_40_ONE_WAY = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_40_one_way"), itemSettings -> new Item(new ItemRailModifier(true, false, true, true, RailType.STONE, itemSettings)), CreativeModeTabs.CORE);
+		RAIL_CONNECTOR_50 = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_50"), itemSettings -> new Item(new ItemRailModifier(true, false, true, false, RailType.STONE2, itemSettings)), CreativeModeTabs.CORE);
+		RAIL_CONNECTOR_50_ONE_WAY = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_50_one_way"), itemSettings -> new Item(new ItemRailModifier(true, false, true, true, RailType.STONE2, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_60 = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_60"), itemSettings -> new Item(new ItemRailModifier(true, false, true, false, RailType.EMERALD, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_60_ONE_WAY = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_60_one_way"), itemSettings -> new Item(new ItemRailModifier(true, false, true, true, RailType.EMERALD, itemSettings)), CreativeModeTabs.CORE);
 		RAIL_CONNECTOR_80 = Init.REGISTRY.registerItem(new Identifier(Init.MOD_ID, "rail_connector_80"), itemSettings -> new Item(new ItemRailModifier(true, false, true, false, RailType.IRON, itemSettings)), CreativeModeTabs.CORE);
@@ -168,6 +170,8 @@ public final class Items {
 	public static final ItemRegistryObject RAIL_CONNECTOR_160_ONE_WAY;
 	public static final ItemRegistryObject RAIL_CONNECTOR_20;
 	public static final ItemRegistryObject RAIL_CONNECTOR_20_ONE_WAY;
+	public static final ItemRegistryObject RAIL_CONNECTOR_50;
+	public static final ItemRegistryObject RAIL_CONNECTOR_50_ONE_WAY;
 	public static final ItemRegistryObject RAIL_CONNECTOR_30;
 	public static final ItemRegistryObject RAIL_CONNECTOR_30_ONE_WAY;
 	public static final ItemRegistryObject RAIL_CONNECTOR_200;
